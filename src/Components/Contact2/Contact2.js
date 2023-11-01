4ee4e5e28dc2a3bdea841545c75923c3ad24d66a
Gabriel.Borba00 <gabriel-borba2011@uni9.edu.br>
2023-11-01 07:55:21 -0300

    arrumando divs

--- src/Components/Contact2/Contact2.js
+++ src/Components/Contact2/Contact2.js
@@ -2,12 +2,12 @@ import Wppbrand from '../../Assets/Wpp-brand.png'
 import './Contact2.css';
 const Contaact =() =>{
 return(
-    <a className="contact-icons">
+    <div className="contact-icons">
         <img src = {Wppbrand} alt='wppbrand'>
         </img>
         
 
-    </a >
+    </div >
 
 )
 
